2f7eb9cb74cab8e49fe6fb90f6a7f93ed60304eb
wxs <moyosoreolaleye@gmail.com>
2024-04-02 09:53:35 +0100

    add footer header

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -1,5 +1,6 @@
 import "./AppFooter.css";
 import AppLogo from "../AppLogo/AppLogo";
+import AppIcon from "../svgs/AppIcon";
 
 function AppFooter() {
   const footerLinks = {
@@ -14,48 +15,58 @@ function AppFooter() {
     "GET IN TOUCH @": ["Instagram", "LinkedIn", "Twitter"]
   }
   return (
-    <footer>
-      <div className="items">
-        <div className="left">
-          <div className="top">
-            <AppLogo />
-            <p>We take complex hiring processes - and simplify them. Connecting you to the world's highly qualified talent pool.</p>
-          </div>
-          <div className="link">
-            <h4>LINKS AND REDIRECTS</h4>
-            <div className="buttons">
-              <button>Hire now</button>
-              <button>Apply now</button>
+    <>
+      <footer>
+        <div className="top-section">
+          <div className="footer-content">
+            <h1>Need a job done, and done well? Get started</h1>
+            <div className="icon">
+              <AppIcon />
             </div>
           </div>
         </div>
-        <div className="right">
-          <h1>Connecting the right people to the right businesses.</h1>
-          <div className="footer-links">
-            {
-              Object.entries(footerLinks).map((item, index) => {
-                return <div className="items" key={index}>
-                  <h4 className="label">
-                    {item[0].toUpperCase()}
-                  </h4>
-                  <div className="item">
-                    {item[1]?.map((i, idx) => <p key={idx}>{i}</p>)}
+        <div className="items">
+          <div className="left">
+            <div className="top">
+              <AppLogo />
+              <p>We take complex hiring processes - and simplify them. Connecting you to the world's highly qualified talent pool.</p>
+            </div>
+            <div className="link">
+              <h4>LINKS AND REDIRECTS</h4>
+              <div className="buttons">
+                <button>Hire now</button>
+                <button>Apply now</button>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <h1>Connecting the right people to the right businesses.</h1>
+            <div className="footer-links">
+              {
+                Object.entries(footerLinks).map((item, index) => {
+                  return <div className="items" key={index}>
+                    <h4 className="label">
+                      {item[0].toUpperCase()}
+                    </h4>
+                    <div className="item">
+                      {item[1]?.map((i, idx) => <p key={idx}>{i}</p>)}
+                    </div>
                   </div>
-                </div>
-              })
-            }
+                })
+              }
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="bottom">
-        <p>All rights reserved  by Zwilt</p>
-        <div className="policy">
-          <p>Privacy Policy</p>
-          <p>Terms and Conditions</p>
+        <div className="bottom">
+          <p>All rights reserved  by Zwilt</p>
+          <div className="policy">
+            <p>Privacy Policy</p>
+            <p>Terms and Conditions</p>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   )
 }
 
